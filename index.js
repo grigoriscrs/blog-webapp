fbac5338299f2654db194c85876997f5349343d5
grigoriscrs <gregochrist1@gmail.com>
2024-10-20 18:08:14 +0300

    created some styling, and completed post creation mechanism

--- index.js
+++ index.js
@@ -3,30 +3,31 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+var postId = 0;
+const posts = [];
+
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req,res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {posts: posts});
 });
 
-app.get("/about", (req,res)=> {
-  res.render("about.ejs");
+app.get("/post/edit", (req,res)=> {
+  res.render("editpost.ejs", {posts: posts});
 });
 
-app.get("/newpost", (req,res)=> {
+app.get("/post/new", (req,res)=> {
   res.render("newpost.ejs");
 });
 
 app.post("/submit",(req,res)=>{
-  const postTitle = req.body["postTitle"];
-  const postBody = req.body["postBody"];
-  console.log(postTitle);
-  res.render("index.ejs", {
-    title: postTitle,
-    body: postBody
-  });
+  var title = req.body["postTitle"];
+  var body = req.body["postBody"];
+  posts.push({title, body, postId});
+  postId++;
+  res.redirect("/");
 });
 
 app.listen(port, ()=> {
